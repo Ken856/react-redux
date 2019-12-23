e08c09d2ac2d680c2dfa977b56876b18a033b8e5
Ken Kato <ken090401@gmail.com>
2019-12-23 16:39:10 +0900

    inplement action and action creater

---
+++ hello-world/src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCRIMENT = 'INCRIMENT'
+export const DECRIMENT = 'DECRIMENT'
+
+export const increment = () => ({
+    type: INCRIMENT
+})
+
+export const decriment = () = ({
+    type: DECRIMENT
+})
