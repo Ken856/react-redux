1468613ca119b7799786cde8b5201d4a39ef2590
Ken Kato <ken090401@gmail.com>
2019-12-23 14:14:54 +0900

    create class component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-function App() {
-  const greeting = "Hi, Tom!!"
-  const dom = <h1 className="foo">{greeting}</h1>;
-  return (
-    dom
-  );
+// function App() {
+//   const greeting = "Hi, Tom!!"
+//   const dom = <h1 className="foo">{greeting}</h1>;
+//   return (
+//     dom
+//   );
+// }
+
+const App = () => {
+  return(
+    <div>
+      <Cat/>
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow</div>
 }
 
 export default App;
